2fc5a05e67ff9abb8a75aa3f1233500e6ff761ba
xuzhang <xuzhang@superbytech.com>
2025-08-24 22:19:12 +0800

    短剧聚合页面适配+

--- lib/pages/dj_video/dj_video_view.dart
+++ lib/pages/dj_video/dj_video_view.dart
@@ -1,10 +1,10 @@
+import 'package:base_object/core/components/cu_nav_bar/cu_nav_bar_view.dart';
+import 'package:base_object/pages/dj_video/dj_video_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_pangrowth/flutter_pangrowth.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
-
-import 'dj_video_controller.dart';
 
 class DjVideoView extends StatefulWidget {
   const DjVideoView({super.key});
@@ -27,6 +27,7 @@ class _DjVideoViewState extends State<DjVideoView> {
       value: SystemUiOverlayStyle.dark,
       child: Scaffold(
         appBar: AppBar(elevation: 0, toolbarHeight: 0, backgroundColor: Colors.black),
+        bottomNavigationBar: CuNavBarView(),
         body: PangrowthVideo.drawHomeView(viewWidth: 1.sw, viewHeight: 1.sh - ScreenUtil().statusBarHeight),
       ),
     );
