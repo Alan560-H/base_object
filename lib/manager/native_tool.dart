05ad87455c89ffb06c4866b4ada512047108dfcd
Alan <1107575251@qq.com>
2025-08-31 21:46:00 +0800

    稍微增加点高度

--- lib/manager/native_tool.dart
+++ lib/manager/native_tool.dart
@@ -121,7 +121,7 @@ class NativeTool extends GetxService{
               Get.width,
               170,
               x: 0,
-              y: 20,
+              y: 30,
               backgroundColorStr: '#FFFFFF'
           ),
           ATNativeManager.appIcon(): ATNativeManager.createNativeSubViewAttribute(
