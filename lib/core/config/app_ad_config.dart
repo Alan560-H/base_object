3aebc9bd17d2441b813f68a3dc52aefe8c6c3571
Alan <1107575251@qq.com>
2025-09-08 15:50:43 +0800

    注册包名修改成如意开盒

--- lib/core/config/app_ad_config.dart
+++ lib/core/config/app_ad_config.dart
@@ -2,24 +2,24 @@ import 'package:get/get.dart';
 
 class AppAdConfig extends GetxController{
   static AppAdConfig get instance => Get.find();
-  static const String appidStr = 'a68ada29a8380c';
-  static const String appidkeyStr = 'a18899a8b8b91ea35ba2138f06d22d980';
+  static const String appidStr = 'a68ad7c89b8241';
+  static const String appidkeyStr = 'a1b6181b3c775f9a9bab1d3b473b43d93';
   /// 开屏 id
-  static const String splashID = 'b68ada2e815a1a';
-  static const String splashSceneID = 'b68ada2e815a1a';
+  static const String splashID = 'b68ad7cc040a91';
+  static const String splashSceneID = 'b68ad7cc040a91';
   /// 横幅 id
-  static const String bannerPlacementID = 'b68ada2ea3f454';
-  static const String bannerSceneID = 'b68ada2ea3f454';
+  static const String bannerPlacementID = 'b68ad7cf850414';
+  static const String bannerSceneID = 'b68ad7cf850414';
   /// 插屏id
-  static const String interstitialPlacementID = 'b68ada2e8bca23';
+  static const String interstitialPlacementID = 'b68ad7cf7a44ee';
   /// 插屏场景id
-  static const String interstitialSceneID = 'b68ada2e8bca23';
+  static const String interstitialSceneID = 'b68ad7cf7a44ee';
   /// 原生id
-  static const String nativePlacementID = 'b68ada2e9913cf';
+  static const String nativePlacementID = 'b68ad7cf9152da';
   /// 原生场景id
-  static const String nativeSceneID = 'b68ada2e9913cf';
+  static const String nativeSceneID = 'b68ad7cf9152da';
   /// 激励视频id
-  static const String rewarderPlacementID = 'b68ada2e761da8';
+  static const String rewarderPlacementID = 'b68ad7ca132f0f';
   /// 激励视频场景id
-  static const String rewarderSceneID = 'b68ada2e761da8';
+  static const String rewarderSceneID = 'b68ad7ca132f0f';
 }
