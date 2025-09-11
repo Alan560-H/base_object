6c45ad9ded734da9437a412266bc831aaffc64fe
Alan <1107575251@qq.com>
2025-09-11 14:36:00 +0800

    111

--- lib/core/components/cu_nav_bar/cu_nav_bar_controller.dart
+++ lib/core/components/cu_nav_bar/cu_nav_bar_controller.dart
@@ -1,4 +1,3 @@
-
 import 'dart:developer';
 import 'dart:ffi';
 
@@ -31,12 +30,12 @@ class CuNavBarController extends GetxController {
   var selectedIndex = 0.obs;
   // 创建一个菜单对象
   MenuModel _createMenuModel(
-      int id,
-      String menuName,
-      bool isMock,
-      String defaultIcon,
-      String activeIcon,
-      ) {
+    int id,
+    String menuName,
+    bool isMock,
+    String defaultIcon,
+    String activeIcon,
+  ) {
     return MenuModel(
       id: id,
       menuName: menuName,
@@ -45,35 +44,37 @@ class CuNavBarController extends GetxController {
       activeIcon: activeIcon,
     );
   }
+
   /// 一级页面索引
   RxInt currentPageIndex = 0.obs;
   RxDouble height = 110.h.obs;
   void onTabChange(int index) {
-    try{
+    try {
       currentPageIndex.value = index;
-      switch(index){
+      switch (index) {
         case 0:
           Get.toNamed(AppRoutes.home);
           break;
+        // case 1:
+        //   Get.toNamed(AppRoutes.shortVideo);
+        //   break;
         case 1:
-          Get.toNamed(AppRoutes.shortVideo);
-          break;
-        case 2:
           Get.toNamed(AppRoutes.djVideo);
           break;
-        case 3:
+        case 2:
           Get.toNamed(AppRoutes.invite);
           break;
-        case 4:
+        case 3:
           Get.toNamed(AppRoutes.user);
           break;
       }
-    }catch(e){
+    } catch (e) {
       Utils.logError("切换一级页面失败：$e");
       Get.toNamed(AppRoutes.home);
       currentPageIndex.value = 0;
     }
   }
+
   List<MenuModel> menuModels = [];
   // 定义一个方法来生成 BottomNavigationBarItem 列表
   List<BottomNavigationBarItem> getNavigationItems() {
@@ -84,16 +85,15 @@ class CuNavBarController extends GetxController {
         false,
         ImageConfig.redBagDefatult,
         ImageConfig.redBagActive,
-
       ),
-      _createMenuModel(
-        1,
-        "短视频",
-        true,
-        ImageConfig.videoDefault,
-        ImageConfig.videoActive,
-
-      ),
+      // _createMenuModel(
+      //   1,
+      //   "短视频",
+      //   true,
+      //   ImageConfig.videoDefault,
+      //   ImageConfig.videoActive,
+      //
+      // ),
       _createMenuModel(
         2,
         "短剧",
@@ -101,7 +101,6 @@ class CuNavBarController extends GetxController {
         ImageConfig.shortVideoDefault,
 
         ImageConfig.shortVideoActive,
-
       ),
       _createMenuModel(
         3,
@@ -109,7 +108,6 @@ class CuNavBarController extends GetxController {
         false,
         ImageConfig.inviteDefault,
         ImageConfig.inviteActive,
-
       ),
       _createMenuModel(
         4,
@@ -136,6 +134,7 @@ class CuNavBarController extends GetxController {
       );
     }).toList();
   }
+
   upDataADFn(dynamic event) async {
     try {
       UpDataADForm upDataADForm = UpDataADForm();
@@ -149,17 +148,22 @@ class CuNavBarController extends GetxController {
       String adsourceId = event?['extraMap']?['adsource_id'];
       // 2. 拼接 extra 字符串（用原始值的字符串形式，避免类型问题）
       String userId = UserInfo.instance.userModel.id.toString();
-      upDataADForm.extra = "userid_${userId}_type_2_amount_${adSourcePrice ?? 0}_time_0";
+      upDataADForm.extra =
+          "userid_${userId}_type_2_amount_${adSourcePrice ?? 0}_time_0";
       upDataADForm.transId = event?['extraMap']?['id'];
-      upDataADForm.amount = (amount!/1000);
+      upDataADForm.amount = (amount! / 1000);
       upDataADForm.adsourceId = adsourceId;
       upDataADForm.reqId = reqId;
 
-      upDataADForm.sign=Utils.generateEncryptedString(userId: userId,reqId:reqId,adsourceId: adsourceId);
+      upDataADForm.sign = Utils.generateEncryptedString(
+        userId: userId,
+        reqId: reqId,
+        adsourceId: adsourceId,
+      );
       Utils.logError("横幅广告凑成的字符串${upDataADForm.toJson()}");
       // 4. 原有进度逻辑不变（保留你的业务逻辑）
       int pross = amount.toInt();
-      if(pross>Store.instance.getFkConfig.wactchMaxAmountV1){
+      if (pross > Store.instance.getFkConfig.wactchMaxAmountV1) {
         CheckDeviceForm checkDeviceForm = CheckDeviceForm();
         checkDeviceForm.oaid = await FlutterAndroidOaidPlugin.getOAID();
         checkDeviceForm.userId = UserInfo.instance.userModel.id;
@@ -171,11 +175,13 @@ class CuNavBarController extends GetxController {
       Utils.logError("上报副广失败：$e");
     }
   }
+
   // 用于标记是否已处理跳转（避免重复跳转）
   final bool _hasShow = false;
+
   /// 订阅 ListenerTool 的开屏广告事件
   void _bannerEvent() async {
-    if(!Get.isRegistered<ListenerTool>()){
+    if (!Get.isRegistered<ListenerTool>()) {
       Get.put(ListenerTool());
     }
     // ever：持续监听 splashEvent 的变化（广告状态更新时触发）
@@ -188,60 +194,71 @@ class CuNavBarController extends GetxController {
       Utils.logError("收到横幅广告事件：$eventType，广告位ID：$placementID，事件参数：$event");
       // 根据事件类型执行业务逻辑
       switch (eventType) {
-      // 横幅广告加载失败
+        // 横幅广告加载失败
         case "BannerStatus.bannerAdFailToLoadAD":
           Utils.logError("横幅广告加载失败");
           height.value = 60.h;
           break;
-      // 横幅广告加载完成
+        // 横幅广告加载完成
         case "BannerStatus.bannerAdDidFinishLoading":
-          if(!Get.isRegistered<BannerTool>()){
+          if (!Get.isRegistered<BannerTool>()) {
             Get.put(BannerTool());
           }
           bool a = await BannerTool.to.bannerAdReady();
           Utils.logError("横幅广告加载完成,$a");
-          height.value=110.h;
+          height.value = 110.h;
           BannerTool.to.showAdInPosition();
           break;
 
-      /// 横幅广告自动刷新成功
+        /// 横幅广告自动刷新成功
         case "BannerStatus.bannerAdAutoRefreshSucceed":
           height.value = 110.h;
-          Utils.logError("${Jiffy.now().format(pattern: "yyyy-MM-dd HH:mm:ss")}好招术${event['extraMap']}");
-          if(Get.isRegistered<UserInfo>()){
-              upDataADFn(event);
+          Utils.logError(
+            "${Jiffy.now().format(pattern: "yyyy-MM-dd HH:mm:ss")}好招术${event['extraMap']}",
+          );
+          if (Get.isRegistered<UserInfo>()) {
+            upDataADFn(event);
           }
 
           break;
-      /// 横幅广告被点击
+
+        /// 横幅广告被点击
         case "BannerStatus.bannerAdDidClick":
           height.value = 110.h;
           Utils.logError("横幅广告被点击");
           break;
-      /// 横幅广告触发深度链接（跳转至指定页面/内容）
+
+        /// 横幅广告触发深度链接（跳转至指定页面/内容）
         case "BannerStatus.bannerAdDidDeepLink":
           Utils.logError("横幅广告触发深度链接（跳转至指定页面/内容）");
           break;
-      /// 点击
+
+        /// 点击
         case "BannerStatus.splashDidClick":
           Utils.logError("横幅广告点击");
           break;
-      /// 横幅广告展示成功
+
+        /// 横幅广告展示成功
         case "BannerStatus.bannerAdDidShowSucceed":
-          Utils.logError("${Jiffy.now().format(pattern: "yyyy-MM-dd HH:mm:ss")}横幅广告展示成功$event，增加进度");
+          Utils.logError(
+            "${Jiffy.now().format(pattern: "yyyy-MM-dd HH:mm:ss")}横幅广告展示成功$event，增加进度",
+          );
           height.value = 110.h;
           break;
-      /// 横幅广告点击关闭按钮
+
+        /// 横幅广告点击关闭按钮
         case "BannerStatus.bannerAdTapCloseButton":
           Utils.logError("横幅广告点击关闭按钮");
           height.value = 110.h;
           break;
-      /// 横幅广告自动刷新失败
+
+        /// 横幅广告自动刷新失败
         case "BannerStatus.bannerAdAutoRefreshFail":
           Utils.logError("横幅广告自动刷新失败");
           height.value = 60.h;
           break;
-      /// 横幅广告未知状态
+
+        /// 横幅广告未知状态
         case "BannerStatus.bannerAdUnknown":
           Utils.logError("横幅广告未知状态");
           height.value = 60.h;
@@ -249,10 +266,11 @@ class CuNavBarController extends GetxController {
       }
     });
   }
+
   @override
   void onInit() async {
     Utils.logError("banner：${Get.isRegistered<BannerTool>()}");
-    if(!Get.isRegistered<BannerTool>()){
+    if (!Get.isRegistered<BannerTool>()) {
       Get.put(BannerTool());
     }
     // TODO: implement onReady
@@ -260,17 +278,19 @@ class CuNavBarController extends GetxController {
     // 1. 先订阅开屏广告事件（关键：确保事件监听在广告展示前生效）
     _bannerEvent();
     DateTime now = DateTime.now();
-    int timestampMs  = now.millisecondsSinceEpoch;
-    await BannerTool.to.loadBannerWith(
-        {
-          Common.getUserIdKey(): UserInfo.instance.userModel.id,
-          Common.getExtraKey(): "userid_${UserInfo.instance.userModel.id}_type_2_amount_0_time_$timestampMs",
-          ATCommon.isNativeShow() : true,
-          ATCommon.getAdSizeKey(): ATBannerManager.createLoadBannerAdSize(
-              Get.width, Get.width * (50 / 320)),
-          ATBannerManager.getAdaptiveWidthKey(): Get.width,
-          ATBannerManager.getAdaptiveOrientationKey(): ATBannerManager.adaptiveOrientationCurrent(),
-        }
-    );
+    int timestampMs = now.millisecondsSinceEpoch;
+    await BannerTool.to.loadBannerWith({
+      Common.getUserIdKey(): UserInfo.instance.userModel.id,
+      Common.getExtraKey():
+          "userid_${UserInfo.instance.userModel.id}_type_2_amount_0_time_$timestampMs",
+      ATCommon.isNativeShow(): true,
+      ATCommon.getAdSizeKey(): ATBannerManager.createLoadBannerAdSize(
+        Get.width,
+        Get.width * (50 / 320),
+      ),
+      ATBannerManager.getAdaptiveWidthKey(): Get.width,
+      ATBannerManager.getAdaptiveOrientationKey():
+          ATBannerManager.adaptiveOrientationCurrent(),
+    });
   }
 }
