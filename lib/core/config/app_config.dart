2f9c6c50713cf81152358fbf6d404ae71a277a44
Alan <1107575251@qq.com>
2025-09-11 17:01:48 +0800

    1

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -13,10 +13,10 @@ class AppConfig extends GetxController {
   String get appDownUrl => "https://img.ruyimh.com/Rscsgo/app/ruyimh.apk";
 
   /// 获取app 服务协议
-  Uri get protocolUri => Uri.parse("https://www.ruyimh.com/protocol.html");
+  Uri get protocolUri => Uri.parse("https://ysxy.ruyigx.com/protocol.html");
 
   /// 获取app 隐私政策
-  Uri get policyUri => Uri.parse("https://www.ruyimh.com/policy.html");
+  Uri get policyUri => Uri.parse("https://ysxy.ruyigx.com/policy.html");
 
   /// 获取app 53客服
   Uri get serviceUri => Uri.parse(
@@ -28,7 +28,7 @@ class AppConfig extends GetxController {
       "http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=oQa4YkADFXhPbT3FJ0nkvXlhlJ6rb54-&authKey=td6mejWblP%2FX%2BgB231w1oj2KQsxM3NH%2BtwLRUAQT1UEEhox5VO%2B9M9Ep2yyS7ex%2F&noverify=0&group_code=1006103675";
 
   /// 注册中间页地址
-  String get registerUrl => "http://192.168.0.136:8080/invite_register.html";
+  String get registerUrl => "https://ysxy.ruyigx.com/invite_register.html";
 
   /// 私有变量，存储URL配置
 
