e02fabef9f51985a0b62daa94f314887ac865b51
Alan <1107575251@qq.com>
2025-09-11 16:57:14 +0800

    1

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -28,7 +28,7 @@ class AppConfig extends GetxController {
       "http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=oQa4YkADFXhPbT3FJ0nkvXlhlJ6rb54-&authKey=td6mejWblP%2FX%2BgB231w1oj2KQsxM3NH%2BtwLRUAQT1UEEhox5VO%2B9M9Ep2yyS7ex%2F&noverify=0&group_code=1006103675";
 
   /// 注册中间页地址
-  String get registerUrl => "http://192.168.0.136:8080/invite_register.html";
+  String get registerUrl => "https://www.ruyimh.com/invite_register.html";
 
   /// 私有变量，存储URL配置
 
