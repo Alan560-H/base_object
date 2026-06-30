a5e61245c2b7b2c411f7cf38e92ba28a2fc3c410
Alan <1107575251@qq.com>
2026-06-30 11:11:40 +0800

    暂时隐藏激励 ，信息流

--- android/app/src/main/kotlin/com/sanzhimie/MainActivity.kt
+++ android/app/src/main/kotlin/com/sanzhimie/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.xxrj
+package com.sanzhimie
 
 import android.content.Intent
 import android.os.Bundle
--- lib/shared/config/app_ad_config.dart
+++ lib/shared/config/app_ad_config.dart
@@ -3,20 +3,20 @@ class AppAdConfig {
   AppAdConfig._();
 
   /// Taku 应用 ID（[InitTool.initTopon]）
-  static const String appidStr = 'a6a2bda1ad01b0';
+  static const String appidStr = 'a6a02edc510688';
 
   /// Taku App Key
-  static const String appidkeyStr = 'acb9c86f1a0c6e45d7fc0fdbdf593f536';
+  static const String appidkeyStr = 'a9dfbeb796a19b7695dbc0d563734d86a';
 
   /// 横幅广告位
-  static const String bannerPlacementID = 'b6a2bda6aa050a';
+  static const String bannerPlacementID = 'b6a02ee1db6f26';
 
   /// 原生信息流广告位（[NativeTool]）
-  static const String nativePlacementID = 'b6a2bdabf9155f';
+  static const String nativePlacementID = 'b6a033299c2230';
 
   /// 信息流场景 ID（与 placement 一致，供 entryNativeScenario / PlatformNativeWidget）
   static const String nativeSceneID = nativePlacementID;
 
   /// 激励视频广告位
-  static const String rewarderPlacementID = 'b6a3b7682af958';
+  static const String rewarderPlacementID = 'b69fad67a43bb4';
 }
